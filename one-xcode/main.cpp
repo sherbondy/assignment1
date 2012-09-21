37f2791a24cda1fa04fef8e62a07b0c0faeeb0d7
Ethan Sherbondy <sherbondye@gmail.com>
2012-09-21 16:22:31 -0400

    Initial Commit

---
+++ one-xcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  one-xcode
+//
+//  Created by Ethan Sherbondy on 9/21/12.
+//  Copyright (c) 2012 Ethan Sherbondy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
